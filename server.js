72903827a245deefd8a846f2c341b450508a46de
Andrea_Graziosi <andreaggraziosi@gmail.com>
2021-04-22 19:52:32 -0700

    users can view a list of posts

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const handlebars = require('handlebars')
+const bodyParser = require('body-parser');
+
+const port = 3000
+const expressValidator = require('express-validator');
+// Set db
+require('./data/reddit-db');
+
+//server
+const app = express()
+
+// Middleware
+const exphbs  = require('express-handlebars');
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+// Add after body parser initialization!
+app.use(expressValidator());
+
+//controllers
+require('./controllers/posts.js')(app);
+
+
+
+
+
+
+//Routes:
+// app.get('/', (req, res) => {
+//     //   res.send('Hello Booches!')
+//     res.render('posts-index', {})
+//     })
+
+app.get('/posts/new', (req, res) => {
+//   res.send('Hello Booches!')
+res.render('posts-new', {})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
